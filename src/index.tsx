fc805f4158d59c742d6d2b7646f8e2f7aa944f68
vipe4ka-visko4ka <pavel.makk.old@gmail.com>
2020-09-23 01:14:47 +0300

    Setup material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
