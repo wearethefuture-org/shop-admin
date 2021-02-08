58a55a339dcaeee8cec3518d7b10998469cd43ae
anastasiiaboichuk <nastyaboychuc@gmail.com>
2021-02-08 19:26:36 +0200

    Delete redundant code

--- src/components/Forms/Slider-form/Inner-form.tsx
+++ src/components/Forms/Slider-form/Inner-form.tsx
@@ -19,34 +19,18 @@ const InnerForm: React.FC<InnerSliderFormProps & FormikProps<IFormValues>> = (
     const classes = useStyles();
 
     const dragOverHandler = (event: React.DragEvent<HTMLFormElement>) => {
-        console.log('File(s) in drop zone');
-
-        // Prevent default behavior (Prevent file from being opened)
         event.preventDefault();
     }
 
     const dropHandler = (event: React.DragEvent<HTMLFormElement>) => {
-        console.log('File(s) dropped');
-
-        // Prevent default behavior (Prevent file from being opened)
         event.preventDefault();
 
         if (event.dataTransfer.items) {
-            // Use DataTransferItemList interface to access the file(s)
-            for (var i = 0; i < event.dataTransfer.items.length; i++) {
-                // If dropped items aren't files, reject them
+            for (let i = 0; i < event.dataTransfer.items.length; i++) {
                 if (event.dataTransfer.items[i].kind === 'file') {
-                    var file = event.dataTransfer.items[i].getAsFile();
-                    console.log('... file[' + i + '].name = ' + file?.name);
+                    let file = event.dataTransfer.items[i].getAsFile();
                     props.setFieldValue("image", file)
-                    //saveImage( event.dataTransfer.files[i]);
                 }
-            }
-        } else {
-            // Use DataTransfer interface to access the file(s)
-            for (var i = 0; i < event.dataTransfer.files.length; i++) {
-                console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
-
             }
         }
     }
