5dbb5544e3a3ef0757d73e5e49e2d11453f91436
vladyslavs <vladyslavs@default-value.com>
2022-05-20 12:45:31 +0300

    fix retreat from the drop-down menu on the user page 361

--- src/components/AppDataTable/AppDataTable.tsx
+++ src/components/AppDataTable/AppDataTable.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction, ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import DataTable from 'react-data-table-component';
 import Card from '@material-ui/core/Card';
+import { customStylesDataTable } from './CustomStylesDataTable'
 
 import { RootState } from '../../store/store';
 
@@ -30,7 +31,7 @@ const AppDataTable: React.FC<DataTableProps> = ({
   setLimit = () => {},
   paginationServer = false,
   defaultSortFieldId = '',
-  customStyles = '',
+  customStyles = customStylesDataTable,
 }) => {
   const { darkMode } = useSelector((state: RootState) => state.theme);
 
