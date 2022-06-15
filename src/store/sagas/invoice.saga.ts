67edfe8e9e7a6eee4ee133ebb9d00791cd11c788
Andrii <kotsupyr.a@gmail.com>
2022-06-15 10:40:00 +0300

    add success snackbar

--- src/store/sagas/invoice.saga.ts
+++ src/store/sagas/invoice.saga.ts
@@ -1,7 +1,7 @@
 import { SagaIterator } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { generateInvoice, getInvoicesList, removeInvoice } from '../actions/invoice.actions';
-import { failSnackBar } from '../actions/snackbar.actions';
+import { failSnackBar, successSnackBar } from '../actions/snackbar.actions';
 import {
   apiGenerateInvoice,
   apiGetInvoicesList,
@@ -23,6 +23,7 @@ export function* removeInvoiceWorker({ data }: Params): SagaIterator {
   try {
     const invoice = yield call(apiRemoveInvoice, data.invoiceName);
     yield put(removeInvoice(invoice.deletedInvoice));
+    yield put(successSnackBar());
   } catch (e) {
     yield put(failSnackBar(e.message));
   }
