554a5f7d28ca3de1488180811cde8cd630f91746
Andrii <kotsupyr.a@gmail.com>
2022-06-28 20:21:14 +0300

    add slides table wrapper

--- src/components/Containers/Slides-container.tsx
+++ src/components/Containers/Slides-container.tsx
@@ -5,14 +5,27 @@ import useSlides from '../../hooks/useSlides';
 import FormDialog from '../Modals/Slide-modal';
 import useSlidesModal from '../../hooks/useSlidesModal';
 import SliderAnimations from '../SliderAnimations/SliderAnimations';
+import { createStyles, makeStyles, Theme, ThemeOptions } from '@material-ui/core';
+
+const useStyles = makeStyles(
+  (theme: Theme): ThemeOptions =>
+    createStyles({
+      tableWrapper: {
+        padding: theme.spacing(4),
+      },
+    })
+);
 
 const SlidesContainer: React.FC = () => {
+  const classes = useStyles();
   const { data, dispatch } = useSlides();
   const slidesCreateModalData = useSlidesModal();
 
   return (
     <>
-      <SlideTable data={data} dispatch={dispatch} modalData={slidesCreateModalData} />
+      <div className={classes.tableWrapper}>
+        <SlideTable data={data} dispatch={dispatch} modalData={slidesCreateModalData} />
+      </div>
       <FormDialog
         dispatch={dispatch}
         slidesLength={data.length}
