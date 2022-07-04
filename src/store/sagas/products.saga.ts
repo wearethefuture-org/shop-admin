589ba43e92e9477c0b854a8fbe36d23e2a59b0c5
Ihorzharyi <56356305+wearethefuture-org@users.noreply.github.com>
2022-07-04 22:43:34 +0300

    Update products.saga.ts

--- src/store/sagas/products.saga.ts
+++ src/store/sagas/products.saga.ts
@@ -174,7 +174,6 @@ export function* updateProductWorker({
 
 export function* deleteProductWorker({ data: product }: IActions): SagaIterator {
   try {
-    // const charValues = product.characteristicValue.map((value) => value.id);
     yield call(apiDeleteProduct, product.id);
     yield put(deleteProductSuccess(product.id));
     yield put(successSnackBar());
