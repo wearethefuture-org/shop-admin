fd99906fde4142ffd5f1eb5bc425a16cc76d5a37
Andrii <kotsupyr.a@gmail.com>
2022-07-07 11:20:04 +0300

    fix infinite queries

--- src/pages/Invoices/Invoices.tsx
+++ src/pages/Invoices/Invoices.tsx
@@ -1,26 +1,15 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import InvoicesTable from '../../components/Tables/Invoices/InvoicesTable';
-import { fetchInvoicesList } from '../../store/actions/invoice.actions';
-import { RootState } from '../../store/store';
+import useInvoices from '../../hooks/useInvoices';
 
 const Invoices = () => {
   const dispatch = useDispatch();
-  const invoiceData = useSelector((state: RootState) => state.invoice);
-
-  async function fetchData() {
-    await dispatch(fetchInvoicesList());
-  }
-
-  useEffect(() => {
-    if (invoiceData.invoicesList.length === 0) {
-      fetchData();
-    }
-  }, [invoiceData.invoicesList]);
+  const { invoiceList } = useInvoices();
 
   return (
     <InvoicesTable
-      data={invoiceData.invoicesList.map((i) => {
+      data={invoiceList.map((i) => {
         return {
           id: i.id,
           createdAt: i.createdAt,
